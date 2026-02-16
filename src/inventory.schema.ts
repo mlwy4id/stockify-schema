cecaad8393074797ce0915625fbefdb1ffd47635
mlwy4id <azmiimaulawiysaid67@gmail.com>
2026-02-16 11:09:44 +0800

    add unit id for item

--- src/inventory.schema.ts
+++ src/inventory.schema.ts
@@ -6,10 +6,11 @@ export const CreateItemSchema = z
     initStock: z
       .number("Provide only number for stock!")
       .min(0, "Please provide non-negative number for stock!"),
+    unitId: z.string().min(1, "Please provide item unit"),
   })
   .strict();
 
 export const UpdateItemSchema = CreateItemSchema.partial().refine(
   (data) => Object.keys(data).length > 0,
-  "At least one field must be updated"
+  "At least one field must be updated",
 );
